b5f4f47718fe7d291422b3513c21398e52e8e371
Matias <matiasmarelli122@gmail.com>
2025-03-13 22:54:33 +0000

    Shell the returns

--- SO_1/U1/shell.c
+++ SO_1/U1/shell.c
@@ -6,37 +6,41 @@
 #include <string.h>
 
 int main(){
-    int a;
-    //pid_t pid=fork();
-    const char s[2]="\n";
-    char* token;
-    while(1){
-        
-        pid_t pid=fork();
-        
-        if(pid==0){
-            char* buffer = malloc(sizeof(char)*30);
-            fgets(buffer,30,stdin);
-            token=strtok(buffer,s);
-            printf("Buffer:%s\n Token:%s\n",buffer,token);
-            if(!strcmp(buffer,"exit")){exit(0);}
-
-
-
-            //scanf("%[^\n]",buffer);
-            //fd=open("shell.txt",'w');
-            //write(fd,buffer,100);
-            
-            
-            //bailalo ROCKY : )
-            execl(token,token,NULL);
-            free(buffer);
-        }
-        else{
-            wait(&a);
-        //    printf("\nclear buffer\n");
-        }
-    
+  int a;
+  //pid_t pid=fork();
+  const char s[2]=" ";
+  char* token;
+  char* buffer = malloc(sizeof(char)*30);
+  while(1){
+  pid_t pid=fork();
+  
+  if(pid==0){
+      fgets(buffer,30,stdin);
+      printf("Buffer:%s\n Token:%s\n",buffer,token);
+      if(!strcmp(buffer,"exit")){exit(0);}
+      int i=0;
+      char*args[255];
+      token=strtok(buffer,s);
+      while(token!=NULL){
+        args[i]=token;
+        token=strtok(NULL,s);
+        i++; 
+      }
+      args[i]=NULL;
+      //scanf("%[^\n]",buffer);
+      //fd=open("shell.txt",'w');
+      //write(fd,buffer,100);
+      
+      
+      //bailalo ROCKY : )
+      execv(args[0],args);
     }
-    return 0;
+    else{
+      wait(&a);
+      //printf("\nclear buffer\n");
+    }
+    
+  }
+  free(buffer);
+  return 0;
 }
