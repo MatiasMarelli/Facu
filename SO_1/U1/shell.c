94cc05b09c073654a223a3e748e58d48f89a91a7
Tomas <tomifederici11@gmail.com>
2025-03-14 21:42:13 +0000

    shell matanenes

--- SO_1/U1/shell.c
+++ SO_1/U1/shell.c
@@ -7,40 +7,30 @@
 
 int main(){
   int a;
-  //pid_t pid=fork();
-  const char s[2]=" ";
   char* token;
-  char* buffer = malloc(sizeof(char)*30);
+
   while(1){
-  pid_t pid=fork();
-  
-  if(pid==0){
-      fgets(buffer,30,stdin);
-      printf("Buffer:%s\n Token:%s\n",buffer,token);
-      if(!strcmp(buffer,"exit")){exit(0);}
+    char* buffer = malloc(sizeof(char)*255);
+    fgets(buffer,255,stdin);
+    buffer[strlen(buffer) - 1] = '\0';
+    if(!strcmp(buffer,"exit")){exit(0);}
+    
+    pid_t pid=fork();
+    if(pid==0){
       int i=0;
-      char*args[255];
-      token=strtok(buffer,s);
+      char* args[255];
+      token=strtok(buffer," ");
       while(token!=NULL){
         args[i]=token;
-        token=strtok(NULL,s);
-        i++; 
+        token=strtok(NULL," ");
+        i++;
       }
       args[i]=NULL;
-      //scanf("%[^\n]",buffer);
-      //fd=open("shell.txt",'w');
-      //write(fd,buffer,100);
-      
-      
-      //bailalo ROCKY : )
-      execv(args[0],args);
-    }
-    else{
+      if(execv(args[0],args) == -1){kill(getpid(),SIGKILL);}
+    } else {
       wait(&a);
-      //printf("\nclear buffer\n");
     }
-    
+    free(buffer);
   }
-  free(buffer);
   return 0;
 }
