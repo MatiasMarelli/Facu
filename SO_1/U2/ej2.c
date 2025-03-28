dcfa086cdfc33533384b2fff8e240d6dea7bc7fb
Matias <matiasmarelli122@gmail.com>
2025-03-28 00:43:41 +0000

    ej2

--- SO_1/U2/ej2.c
+++ SO_1/U2/ej2.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 void a(){
   char buffer[255];
@@ -40,8 +41,28 @@ void e(){
   }
 }
 
+void f (){
+  int *a=malloc(sizeof(char)*2^50);
+  //La memoria no cambia (supongo q el sistema operativo no debe ceder la memoria salvo que sea utilizada y/o se empiece a llenar)
+  printf("\nPid:%d\n",getpid());
+  while(1);
+}
+
+void g (){
+  pid_t pid=fork();
+  if(!pid){
+    printf("\nPid:%d\n",getpid());
+    printf("sleeping...\n");
+    sleep(10); 
+    while(1);
+    execl("./test","./test",NULL);
+  }else{
+    wait(0);
+    printf("Termino Hijo\n");
+  }
+}
 
 int main(){  
-  e();
+  g();
   return 0;
 }
