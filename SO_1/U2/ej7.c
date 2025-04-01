1a41e66364f8612ce35e6f73e2f30d3d37061273
Matias <matiasmarelli122@gmail.com>
2025-04-01 17:39:05 +0000

    Shell finished

--- SO_1/U2/ej7.c
+++ SO_1/U2/ej7.c
@@ -8,7 +8,7 @@
 
 
 void exe(char** args){
-  if(execv(args[0],args) == -1){kill(getpid(),SIGKILL);}
+  if(execvp(args[0],args) == -1){printf("ERROR, arg:%s",args[0]);kill(getpid(),SIGKILL);}
 }
 
 int main(){
@@ -20,26 +20,30 @@ int main(){
     fgets(buffer,255,stdin);
     buffer[strlen(buffer) - 1] = '\0';
     if(!strcmp(buffer,"exit")){exit(0);}
-    //dup2(1,STDOUT_FILENO);
-
-
+    
     pid_t pid=fork();
     
     if(pid==0){
-      open("TEMP.txt",'r');
+      
       int fd;
       int i=0;
       int flag=0;
       char* args[255];
+      
+      FILE* file_temp= fopen("TEMP.txt","w+");
+      int temp = fileno(file_temp); 
       token=strtok(buffer," ");
       
+      char* buff= calloc(sizeof(char),255);
       while(token!=NULL){
+        //printf("i: %d, TOKEN %s\n",i,token);
         args[i]=token;
         token=strtok(NULL," ");
         i++;
         //if(!strcmp(">",token)) flag=0;
         if(!strcmp(args[i-1],"|") || token == NULL){
-          printf("\n\t | \n");
+          
+          //printf("\n\t | \n");
           int pipeline[2];
           pipe(pipeline);
           pid_t nieto = fork();
@@ -47,44 +51,81 @@ int main(){
             close(pipeline[0]);
             if(flag == 0){
               if(token==NULL){
-                printf("CASO 0\n");
+                //printf("CASO 0\n");
                 args[i]=NULL;
               }else{
-                printf("CASO 0\n");
+                //printf("CASO 0\n");
                 args[i-1]=NULL;
               }
-              for(int j=0;j<i;j++){
+             /*  for(int j=0;j<i;j++){
                 printf("j:%d, argv:%s\n",j,args[j]);
-              }
+              } */
             }else{
-              printf("CASO 1\n");
-              printf("openat: %d\n",openat(fd,"TEMP.txt",'r'));
-              strcpy(args[i++],"TEMP");  //fdopen(fd,"r");
-              args[i++] = NULL;
-              for(int j=0;j<i;j++){
-                printf("j:%d, argv:%s\n",j,args[j]);
+              if(token==NULL){
+
+                
+                //printf("CASO 1\n");
+                
+                lseek(temp,0,SEEK_SET);
+                write(temp,buff,strlen(buff));
+                //printf("write: %d\n",);
+                args[i]="TEMP.txt";
+                //char msg[256] = "TEMP.txt";
+                //strcpy(args[i],msg);//fdopen(fd,"r");
+                //args[i]=msg;
+                // i++;
+                
+                args[++i] = NULL;
+              }else{
+                lseek(temp,0,SEEK_SET);
+                write(temp,buff,strlen(buff));
+                args[i-1]="TEMP.txt";
+                args[i]=NULL;
               }
+
+               for(int j=0;j<=i;j++){
+                printf("j:%d, argv:%s\n",j,args[j]);
+                //args[j]=NULL;
+              } 
             }
-            //if(token != NULL) dup2(pipeline[1],1);
-            dup2(pipeline[1],1);
+
+            if(token != NULL) dup2(pipeline[1],1);
+            //dup2(pipeline[1],1);
+
             exe(args);
             
           }else{
-            char buff[255];
             wait(NULL);
             close(pipeline[1]);
-            fd=dup(pipeline[0]);
-            read(pipeline[0],buff,255);
-            printf("BUFFER:%s\n",buff);
+            //fd=dup(pipeline[0]);
+            //printf("PIPE: %d\n",fcntl(pipeline[0], F_GETFD));
+            lseek(pipeline[0],0,SEEK_SET);
+            int nbytes=read(pipeline[0],buff,255);
+            //printf("BUFFER (Pre read):%s\n",buff);
+            //printf("NBYTES = %d\n",nbytes);
+            //printf("TEMP: %d\n",fcntl(temp, F_GETFD));
+            //printf("write: %d\n",write(temp,buff,nbytes));
+
+            //printf("BUFFER:%s\n",buff);
             
             //close(pipeline[0]);
+            if(token==NULL){
+              lseek(temp,0,SEEK_SET);
+              write(temp,buff,strlen(buff));
+              //printf("write: %d\n",);
+            }
           }
+          close(pipeline[0]);
+          close(pipeline[1]);
+          
           flag++;
-         
+          //printf("TOKEN DESPUES DE SALIR DE ÑIETO:%s",token);
           i=0;
+          //token=strtok(NULL," ");
         }
       }
-      printf("END WHILE\n");
+      free(buff);
+     // printf("END WHILE\n");
      /*  if(flag==0){
         args[i]=NULL;
       }else{
@@ -121,11 +162,12 @@ int main(){
       //}
       //sleep(1);
       
-
+      //remove("TEMP.txt");
     }else {
       wait(&a);
     }
     free(buffer);
+    //remove("TEMP.txt");
   }
 
 
