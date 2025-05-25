6b81ef7f16c2d40fe854862ab03c86a1e95f4b93
Matias <matiasmarelli122@gmail.com>
2025-05-25 19:43:36 +0000

    ej3-P4

---
+++ SO_1/U5/ej3_p.c
@@ -0,0 +1,62 @@
+#include <omp.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+
+
+#define N 500000000
+#define T 12
+double min=0;
+double* arr;
+
+double* rand_arr(){
+    double* arr=malloc(sizeof(double)*N);
+    for (int i = 0; i < N; i++){
+      arr[i]= ((-1)^(rand()%2))*((rand()%100240)*1.0)/((rand()%10*1.0)+1);
+    } 
+    return arr;
+  }
+
+void print (){
+  for(int i=0;i<N;i++){
+
+    printf("%f ",arr[i]);
+  }
+  printf("\n");
+}
+
+
+void min_search(int id){
+  
+  unsigned long long int start=id*(N/T);
+  unsigned long long int idx=0;
+  for(;idx<N/T;idx++){
+    #pragma omp critical
+    {
+      if(arr[idx+start]<min){
+        min =arr[idx+start];
+        
+      }
+    }
+    
+  }
+}
+
+void min_arr(){
+  #pragma omp parallel num_threads(T)
+  {
+    int id = omp_get_thread_num();
+    //printf("ID:%d\n",id);
+    min_search(id);
+  }
+  printf("Min:%f\n",min);
+}
+
+int main(){
+  arr=rand_arr(); 
+  printf("Termine de hacer el arrroz\n");
+  min=arr[0];
+  min_arr();
+  free(arr);
+  return 0;
+}
