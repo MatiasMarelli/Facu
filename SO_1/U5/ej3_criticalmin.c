98d920336e904b8f5f3099c6f5d6a338024d81d7
Matias <matiasmarelli122@gmail.com>
2025-05-26 19:10:00 +0000

    no avance un carajo

--- SO_1/U5/ej3_criticalmin.c
+++ SO_1/U5/ej3_criticalmin.c
@@ -1,15 +1,14 @@
 #include <omp.h>
 #include <stdlib.h>
 #include <math.h>
-#include <stdio.h>
 #include "timing.h"
+#include <stdio.h>
 
 
 #define N 500000000
 #define T 6
-
+double min=0;
 double* arr;
-double min[T];
 
 double* rand_arr(){
     double* arr=malloc(sizeof(double)*N);
@@ -32,59 +31,35 @@ void min_search(int id){
   
   unsigned long long int start=id*(N/T);
   unsigned long long int idx=0;
-  min[id]=arr[start];
   for(;idx<N/T;idx++){
-    if(arr[idx+start]<min[id]){
-      min[id] =arr[idx+start];
+    #pragma omp critical
+    {
+      if(arr[idx+start]<min){
+        min =arr[idx+start];
+        
+      }
     }
     
-    
   }
-}
-
-double minimun(double* arr){
-  double min=arr[0];
-  for(int i=0;i<T;i++){
-    if(arr[i]<min)
-      min=arr[i];
-  }
-  return min;
 }
 
 void min_arr(){
-  unsigned long long int start;
-    
   #pragma omp parallel num_threads(T)
   {
     int id = omp_get_thread_num();
     //printf("ID:%d\n",id);
-    //min_search(id);
-    #pragma omp private(start,id)
-    {
-
-      start=id*(N/T);   
-      
-      min[id]=arr[start];
-      #pragma omp for
-      for(unsigned long long idx=0;idx<N;idx++){
-        if(arr[idx]<min[id]){
-          min[id] =arr[idx];
-        }
-      }
-    }
+    min_search(id);
   }
-  double global_min= minimun(min);
-  printf("Min:%f\n",global_min);
+  printf("Min:%f\n",min);
 }
 
 int main(){
   arr=rand_arr(); 
   printf("Termine de hacer el arrroz\n");
+  min=arr[0];
   float time;
   TIME_void(min_arr(),&time);
-  
+
   free(arr);
-  
   return 0;
-  
 }
